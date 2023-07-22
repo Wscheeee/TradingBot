b781714e674800a722833658b7a7c7c01d36697e
Georgemburu <snooppythedj@gmail.com>
2023-07-22 21:09:30 +0300

    replace 🟧 with ⏐

--- Telegram/message_templates/trade_detection/sendTradeUpdateDetectedMessage_toUser.js
+++ Telegram/message_templates/trade_detection/sendTradeUpdateDetectedMessage_toUser.js
@@ -26,7 +26,7 @@ module.exports.sendTradeSizeUpdateDetectedMessage_toUser = async function ({
         bot.sendMessage(chatId,
             `🚨 Trade Update Detected: Size 🟧
 ${trader_username} ⏐ ${position_pair} ⏐ ${position_direction} ⏐ x${position_leverage}
-${position_entry_price} 🟧 ${change_by} 🟧 ${new DecimalMath(change_percentage).truncateToDecimalPlaces(2).getResult()}%`
+${position_entry_price} | ${change_by} | ${new DecimalMath(change_percentage).truncateToDecimalPlaces(2).getResult()}%`
         );
     }catch(error){
         error.message = `${FUNCTION_NAME} ${error.message}`;
@@ -55,9 +55,8 @@ module.exports.sendTradeLeverageUpdateDetectedMessage_toUser = async function ({
     try{
         bot.sendMessage(chatId,
             `🚨 Trade Update Detected: Leverage 🟧
-
-${trader_username}⏐${position_pair}⏐${position_direction}⏐x${position_leverage}
-${new DecimalMath(position_entry_price).truncateToDecimalPlaces(5).getResult()}🟧${change_by}🟧${new DecimalMath(change_percentage).truncateToDecimalPlaces(2).getResult()}%`
+${trader_username} ⏐ ${position_pair} ⏐ ${position_direction} ⏐ x${position_leverage}
+${new DecimalMath(position_entry_price).truncateToDecimalPlaces(5).getResult()} ⏐ ${change_by} ⏐ ${new DecimalMath(change_percentage).truncateToDecimalPlaces(2).getResult()}%`
         );
     }catch(error){
         error.message = `${FUNCTION_NAME} ${error.message}`;
--- Telegram/message_templates/trade_execution/sendTradeUpdateExecutedMessage_toUser.js
+++ Telegram/message_templates/trade_execution/sendTradeUpdateExecutedMessage_toUser.js
@@ -26,7 +26,7 @@ module.exports.sendTradeUpdateSizeExecutedMessage_toUser = async function ({
         bot.sendMessage(chatId,
             `🚨 Trade Update Executed: Size 🟧
 ${trader_username} ⏐ ${position_pair} ⏐ ${position_direction} ⏐ x${position_leverage}
-${new DecimalMath(position_entry_price).truncateToDecimalPlaces(5).getResult()}🟧${change_by} 🟧 ${new DecimalMath(change_percentage).truncateToDecimalPlaces(2).getResult()}%`
+${new DecimalMath(position_entry_price).truncateToDecimalPlaces(5).getResult()} ⏐ ${change_by} ⏐ ${new DecimalMath(change_percentage).truncateToDecimalPlaces(2).getResult()}%`
         );
     }catch(error){
         error.message = `${FUNCTION_NAME} ${error.message}`;
@@ -56,7 +56,7 @@ module.exports.sendTradeLeverageUpdateExecutedMessage_toUser = async function ({
         bot.sendMessage(chatId,
             `🚨 Trade Update Executed: Leverage 🟧
 ${trader_username} ⏐ ${position_pair} ⏐ ${position_direction} ⏐ x${position_leverage}
-${new DecimalMath(position_entry_price).truncateToDecimalPlaces(2).getResult()} 🟧 ${change_by} 🟧 ${new DecimalMath(change_percentage).truncateToDecimalPlaces(2).getResult()}%`
+${new DecimalMath(position_entry_price).truncateToDecimalPlaces(2).getResult()} ⏐ ${change_by} ⏐ ${new DecimalMath(change_percentage).truncateToDecimalPlaces(2).getResult()}%`
         );
     }catch(error){
         error.message = `${FUNCTION_NAME} ${error.message}`;
